a8e7d78ebe736afc6f7f9d76a5e4039df405d323
VJ-Vijay77 <vijaydinesh77vj@gmail.com>
2022-08-16 18:06:44 +0530

    done

--- main.go
+++ main.go
@@ -16,4 +16,46 @@ func main() {
 	defer conn.Close()
 
 	fmt.Println("Successfully Connected to our RabbitMQ Instance")
+
+
+	ch, err := conn.Channel()
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"TestQueue",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	fmt.Println(q)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = ch.Publish(
+		"",
+		"TestQueue",
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte("Hello World 2"),
+		},
+	)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+    fmt.Println("Successfully Published Message to Queue")
 }
+
+
+
+
+
