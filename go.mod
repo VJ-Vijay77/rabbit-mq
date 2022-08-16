e3e267ed26d93f7ee39dcaa448870554568787fe
VJ-Vijay77 <vijaydinesh77vj@gmail.com>
2022-08-16 17:58:14 +0530

    rabbitMQ

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/VJ-Vijay77/rabbit-mq
+
+go 1.19
+
+require github.com/streadway/amqp v1.0.0
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/streadway/amqp"
+)
+
+func main() {
+	fmt.Println("Go RabbitMQ Tutorial")
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		fmt.Println(err)
+		panic(1)
+	}
+	defer conn.Close()
+
+	fmt.Println("Successfully Connected to our RabbitMQ Instance")
+}
